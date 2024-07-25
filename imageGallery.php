be1059aa7132192d4769b46b92a5cdc19366d140
Hash <hasithasuranjana12@gmail.com>
2024-07-25 11:38:37 +0530

    idea is completed!

--- imageGallery.php
+++ imageGallery.php
@@ -7,21 +7,69 @@
     <title>Image Gallery</title>
 </head>
 <body>
+    <?php 
+
+        if(isset($_POST["Upload"])) {
+
+            $errors = [] ;
+
+            $fileName = $_FILES['file-input']['name'];
+            $fileSize = $_FILES['file-input']['size'];
+            $fileType = $_FILES['file-input']['type'];
+            $tmpName  = $_FILES['file-input']['tmp_name'];
+
+            $uploadTo = 'ImageStore/';
+
+            if($fileType !='image/jpeg') {
+                $errors[] = 'Only Jpg files can be uploaded !';
+            }elseif ($fileSize > 5000000){
+                $errors[] = 'File Size is too high !';
+            }elseif($fileType !='image/jpeg' && $fileSize > 5000000) {
+                $errors[] = "Doesn't match selected !";
+            }
+
+            if(empty($errors)) {
+                move_uploaded_file($tmpName, $uploadTo.$fileName);
+            }
+        }
+    
+    
+    ?>
 
     <div class="upload">
 
         <div class="upload-items">
 
-            <input type="button" value="Upload">
+            
 
             <form action="imageGallery.php" method="post" enctype="multipart/form-data">
-                <input type="file"  id="file-input">
-                <input type="button" value="Upload">
+                <input type="file"  name="file-input">
+                
+                <input type="submit" name="Upload">
+
             </form>
 
         </div>
-        
+
+        <?php 
+                    if(isset($_POST["Upload"])) {
+                        if(!(empty($errors))) {
+                            foreach($errors as $error) {
+                                echo "{$error}";
+                            }
+                            
+                        }else {
+                            echo  "<img src = '". $uploadTo.$fileName ."' alt = error />" ;
+                        }
+                    }
+
+                ?>
+
     </div>
+
+    
+
+    <script src="imageGallery.js"></script>
     
 </body>
 </html>
