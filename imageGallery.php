bb3dec4259c15f9f24cc51c79564487f7bb790fb
Hash <hasithasuranjana12@gmail.com>
2024-07-25 12:08:15 +0530

    updated files

--- imageGallery.php
+++ imageGallery.php
@@ -3,15 +3,14 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <link rel="stylesheet" href="ImageGallery.css">
     <title>Image Gallery</title>
 </head>
 <body>
     <?php 
-
-        if(isset($_POST["Upload"])) {
-
-            $errors = [] ;
+        if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST["Upload"])) {
+            $errors = [];
 
             $fileName = $_FILES['file-input']['name'];
             $fileSize = $_FILES['file-input']['size'];
@@ -20,56 +19,51 @@
 
             $uploadTo = 'ImageStore/';
 
-            if($fileType !='image/jpeg') {
-                $errors[] = 'Only Jpg files can be uploaded !';
-            }elseif ($fileSize > 5000000){
-                $errors[] = 'File Size is too high !';
-            }elseif($fileType !='image/jpeg' && $fileSize > 5000000) {
-                $errors[] = "Doesn't match selected !";
+            if ($fileType !== 'image/jpeg') {
+                $errors[] = 'Only JPG files can be uploaded!';
+            }
+            if ($fileSize > 5000000) {
+                $errors[] = 'File size is too high!';
             }
 
-            if(empty($errors)) {
-                move_uploaded_file($tmpName, $uploadTo.$fileName);
+            if (empty($errors)) {
+                if (move_uploaded_file($tmpName, $uploadTo . basename($fileName))) {
+                    echo "<div class='alert alert-success'>File uploaded successfully!</div>";
+                } else {
+                    $errors[] = 'There was an error uploading your file.';
+                }
             }
         }
-    
-    
     ?>
 
-    <div class="upload">
-
+    <div class="container-fluid pt-10">
         <div class="upload-items">
-
-            
-
             <form action="imageGallery.php" method="post" enctype="multipart/form-data">
-                <input type="file"  name="file-input">
-                
-                <input type="submit" name="Upload">
-
+                <input type="file" name="file-input" class="form-control mb-3" required>
+                <input type="submit" name="Upload" value="Upload" class="btn btn-primary">
             </form>
-
         </div>
 
         <?php 
-                    if(isset($_POST["Upload"])) {
-                        if(!(empty($errors))) {
-                            foreach($errors as $error) {
-                                echo "{$error}";
-                            }
-                            
-                        }else {
-                            echo  "<img src = '". $uploadTo.$fileName ."' alt = error />" ;
-                        }
+            if (!empty($errors)) {
+                foreach ($errors as $error) {
+                    echo "<div class='alert alert-danger'>" . htmlspecialchars($error) . "</div>";
+                }
+            } elseif (isset($_POST["Upload"]) && empty($errors)) {
+                
+                $imageList = scandir("ImageStore/") ;
+
+                foreach ($imageList as $image) {
+
+                    if(substr($image,strlen($image)-3) == "jpg") {
+                        echo "<img src='" . htmlspecialchars($uploadTo . $image) . "' alt='" . htmlspecialchars($fileName) . "' class='img-fluid mt-3'/>";
                     }
-
-                ?>
-
+                }
+            }
+        ?>
     </div>
 
-    
-
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
     <script src="imageGallery.js"></script>
-    
 </body>
 </html>
